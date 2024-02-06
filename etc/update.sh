8e2a04066f2b96e517b339a68591c8879f019cb2
Aaruni Kaushik <akaushik@mathematik.uni-kl.de>
2024-02-06 11:12:44 +0100

    Update user and repo in etc files

--- etc/update.sh
+++ etc/update.sh
@@ -2,17 +2,17 @@
 set -e
 
 # fetch latest changes
-cd /srv/www/www-mathe-oscar/data/oscar-website/
+cd /srv/www/www-oscar-book/data/book.oscar-system.org/
 git fetch --all --prune
 git checkout --force gh-pages
 git reset --hard origin/gh-pages
 
 # add webhook secret
-cat /srv/www/www-mathe-oscar/data/webhook.secret >> .htaccess
+cat /srv/www/www-oscar-book/data/webhook.secret >> .htaccess
 
 # install gems
 bundle config set --local path 'vendor/bundle'
 bundle install
 
 # run jekyll
-bundle exec jekyll build --config _config.yml,_config_production.yml -d /srv/www/www-mathe-oscar/data/http
+bundle exec jekyll build --config _config.yml,_config_production.yml -d /srv/www/www-oscar-book/data/http
